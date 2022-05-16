6c22eaf0742b27616ffeb093c74ce8b5fff61eb9
leenwood <george200135@gmail.com>
2022-05-16 19:51:04 +0500

    create charset

--- src/Controller/GameMasterController.php
+++ src/Controller/GameMasterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Character;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,5 +18,15 @@ class GameMasterController extends AbstractController
         return $this->render('game_master/index.html.twig', [
             'controller_name' => 'GameMasterController',
         ]);
+    }
+
+    /**
+     * @Route("/game/char/form", name="app_create_char")
+     */
+
+    public function charForm(Request $request): Response
+    {
+        $tmpChar = new Character();
+        return $this->render('game_master/index.html.twig', []);
     }
 }
