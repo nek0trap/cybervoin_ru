6c9d1422ed3643680d5f7b6c343a52a2d5e8ed23
nek0trap <mityastar01@gmail.com>
2022-05-21 11:23:19 +0300

    add-collection-widget DONE

--- public/js/add-collection-widget.js
+++ public/js/add-collection-widget.js
@@ -2,23 +2,12 @@
 jQuery(document).ready(function () {
     jQuery('.add-another-collection-widget').click(function () {
         var list = $('#char_guns');
-        // Try to find the counter of the list or use the length of the list
-        console.log(list);
         var counter = list.data('widget-counter') || list.children().length;
-        console.log(counter);
-        // grab the prototype template
         var newWidget = list.attr('data-prototype');
-
-        // replace the "__name__" used in the id and name of the prototype
-        // with a number that's unique to your emails
-        // end name attribute looks like name="contact[emails][2]"
         newWidget = newWidget.replace(/__name__/g, counter);
-        console.log(newWidget);
-        // Increase the counter
         counter++;
         // And store it, the length cannot be used if deleting widgets is allowed
         list.data('widget-counter', counter);
-
         // create a new list element and add it to the list
         list.append(newWidget);
     });
