ad364fc25e764050f0d54ef83b512ba65e1590be
dmitry <mityastar01@gmail.com>
2022-05-28 00:56:39 +0300

    pretty form and refactor js

--- public/js/add-collection-widget.js
+++ public/js/add-collection-widget.js
@@ -1,20 +1,24 @@
 // add-collection-widget.js
-jQuery(document).ready(function () {
-    var list = $('#guns-list');
-    var gunCounter = list.data('widget-counter') || list.children().length;
-    jQuery('.add-another-gun').click(function () {
-        var newWidget = '<li class="list-group-item guns-list">' + list.attr('data-prototype') + '</li>';
-        console.log(newWidget)
-        newWidget = newWidget.replace(/__name__/g,gunCounter);
-
-        gunCounter++;
-        // And store it, the length cannot be used if deleting widgets is allowed
-        list.data('widget-counter', gunCounter);
-        // create a new list element and add it to the list
-        list.append(newWidget);
-    });
-    jQuery('.remove-another-gun').click(function () {
-        gunCounter--;
-        $('.guns-list').last().remove();
-    });
+$(document).ready(function () {
+    createAddRemoveButtonWithId('gear');
+    createAddRemoveButtonWithId('guns');
+    createAddRemoveButtonWithId('cyberware');
 });
+
+function createAddRemoveButtonWithId(buttonId) {
+    var buttonList = $(`#${buttonId}-list`);
+    var buttonCounter = buttonList.data('widget-counter') || buttonList.children().length;
+
+    $(`.add-another-${buttonId}`).click(function () {
+        var newWidget = `<li class="list-group-item ${buttonId}-list">` + buttonList.attr('data-prototype') + `</li>`;
+        newWidget = newWidget.replace(/__name__/g,buttonCounter);
+        buttonCounter++;
+        buttonList.data('widget-counter', buttonCounter);
+        buttonList.append(newWidget);
+    });
+
+    $(`.remove-another-${buttonId}`).click(function () {
+        buttonCounter--;
+        $(`.${buttonId}-list`).last().remove();
+    });
+}
