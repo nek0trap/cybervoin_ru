e078402841d9ed626b3ebf48535f098a4a5f837f
leenwood <george200135@gmail.com>
2022-06-01 00:06:55 +0500

    add in form skill preset field

--- src/Repository/CharacterSkillPresetRepository.php
+++ src/Repository/CharacterSkillPresetRepository.php
@@ -2,32 +2,32 @@
 
 namespace App\Repository;
 
-use App\Entity\CharacterStatsPreset;
+use App\Entity\CharacterSkillPreset;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\OptimisticLockException;
 use Doctrine\ORM\ORMException;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
- * @extends ServiceEntityRepository<CharacterStatsPreset>
+ * @extends ServiceEntityRepository<CharacterSkillPreset>
  *
- * @method CharacterStatsPreset|null find($id, $lockMode = null, $lockVersion = null)
- * @method CharacterStatsPreset|null findOneBy(array $criteria, array $orderBy = null)
- * @method CharacterStatsPreset[]    findAll()
- * @method CharacterStatsPreset[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ * @method CharacterSkillPreset|null find($id, $lockMode = null, $lockVersion = null)
+ * @method CharacterSkillPreset|null findOneBy(array $criteria, array $orderBy = null)
+ * @method CharacterSkillPreset[]    findAll()
+ * @method CharacterSkillPreset[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class StatCharRepository extends ServiceEntityRepository
+class CharacterSkillPresetRepository extends ServiceEntityRepository
 {
     public function __construct(ManagerRegistry $registry)
     {
-        parent::__construct($registry, CharacterStatsPreset::class);
+        parent::__construct($registry, CharacterSkillPreset::class);
     }
 
     /**
      * @throws ORMException
      * @throws OptimisticLockException
      */
-    public function add(CharacterStatsPreset $entity, bool $flush = true): void
+    public function add(CharacterSkillPreset $entity, bool $flush = true): void
     {
         $this->_em->persist($entity);
         if ($flush) {
@@ -39,7 +39,7 @@ class StatCharRepository extends ServiceEntityRepository
      * @throws ORMException
      * @throws OptimisticLockException
      */
-    public function remove(CharacterStatsPreset $entity, bool $flush = true): void
+    public function remove(CharacterSkillPreset $entity, bool $flush = true): void
     {
         $this->_em->remove($entity);
         if ($flush) {
@@ -75,4 +75,5 @@ class StatCharRepository extends ServiceEntityRepository
         ;
     }
     */
+
 }
